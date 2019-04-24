307daa0a8298b1b978581f4d67de093378ea41e3
wyf <798926264@qq.com>
2019-04-24 08:58:48 +0800

    add bgc && navigation

---
+++ H5/cart/js/index.js
@@ -0,0 +1,13 @@
+$(function(){
+// 初始化fullpage组件
+	$('.container').fullpage({
+		// 设置每一屏的背景颜色
+		sectionsColor:['#fadd67', '#84a2d4', '#ef674d', '#ffeedd', '#d04759', '#84d9ed', '#8ac060'],
+		// 设置对齐方式
+		verticalCentered: false,
+		// 设置导航
+		navigation: true,
+		navigationPosition: 'right',
+
+	});
+});
